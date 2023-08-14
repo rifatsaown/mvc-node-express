bd2b07ab4f00adf5c96102dc5c3b449821802eca
Md. Rifat Hossen Saown <rifatsaown0@gmail.com>
2023-08-15 05:42:16 +0600

    Database Connection Done And Database Instance Added To the request object

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
 import cors from 'cors';
 import express from 'express';
+import { getDbConnection } from './utils/dbConnect';
 
 const app = express();
 
-/* Middlewares */
+/*------------ Middlewares --------------*/
 app.use(cors());
 app.use(express.json());
 
-/* Routes */
+app.use(async (req, res, next) => {
+    try {
+        const database = getDbConnection(); // Access the database instance directly from the request object
+        (req as any).db = database; // Attach the database instance to the request object and (req as any) is used to avoid typescript error
+        console.log('Dtabase Instance Successfully Attached to the Request Object');
+        next();
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+        res.status(500).send({ status: 'error', message: 'Server Error' });
+    }
+});
+
+
+/*--------------- Routes ---------------*/
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import "dotenv/config";
 import app from './app';
+import { connectToDatabase } from "./utils/dbConnect";
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+const startServer = async () => {
+    try{
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+            connectToDatabase();
+        });
+    }catch(err){
+        console.log(`Server Error: ${err}`);
+    }
+};
+
+startServer();
