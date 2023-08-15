58b30c8c2b9ac562de933bca1d9bc298a4fda575
Md. Rifat Hossen Saown <rifatsaown0@gmail.com>
2023-08-15 06:29:14 +0600

    Db Instance Inject Middelware File Created and Transfer Into new file

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,13 @@
 import cors from 'cors';
 import express from 'express';
-import { getDbConnection } from './utils/dbConnect';
+import { dbInstance } from './middleware/dbInstance';
 
 const app = express();
 
 /*------------ Middlewares --------------*/
 app.use(cors());
 app.use(express.json());
-
-app.use(async (req, res, next) => {
-    try {
-        const database = getDbConnection(); // Access the database instance directly from the request object
-        (req as any).db = database; // Attach the database instance to the request object and (req as any) is used to avoid typescript error
-        next();
-    } catch (error) {
-        console.error('Error connecting to the database:', error);
-        res.status(500).send({ status: 'error', message: 'Server Error' });
-    }
-});
+app.use(dbInstance);
 
 /*------------ Home Routes --------------*/
 app.get('/', (req, res) => {
